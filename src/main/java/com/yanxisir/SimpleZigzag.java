58e7643fe5b6b0e2017178c22746445d2b711864
YanxiSir <tanlianfang@didichuxing.com>
2018-09-12 16:48:27 +0800

    add zigzag encoding

---
+++ src/main/java/com/yanxisir/SimpleZigzag.java
@@ -0,0 +1,17 @@
+package com.yanxisir;
+
+public class SimpleZigzag {
+
+    public static void main(String[] args) {
+        System.out.println(cal(2));
+        System.out.println(cal(-2));
+    }
+
+    public static int cal(int n) {
+        System.out.println("=========" + n + "========");
+        System.out.println("n<<1 : " + (n << 1));
+        System.out.println("n>>1 : " + (n >> 31));
+        // 左移-补0；右移-补1
+        return (n << 1) ^ (n >> 31);
+    }
+}
