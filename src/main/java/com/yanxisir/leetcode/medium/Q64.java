d76fb7cd370906cc1e35c3566aae28a221a1933f
YanxiSir <yanxisir@gmail.com>
2020-04-12 23:54:55 +0800

    fix

--- src/main/java/com/yanxisir/leetcode/medium/Q64.java
+++ src/main/java/com/yanxisir/leetcode/medium/Q64.java
@@ -1,18 +1,61 @@
 package com.yanxisir.leetcode.medium;
 
 import com.yanxisir.leetcode.AbstractQ;
+import com.yanxisir.leetcode.zannos.DynamicPrograming;
 
 /**
  * 最小路径和
  * https://leetcode-cn.com/problems/minimum-path-sum/
+ *
  * @author: YanxiSir
  * @Date: 2020/2/21
  * @Sign: 心中田间，木行水上
  */
-@Deprecated
+@DynamicPrograming
 public class Q64 extends AbstractQ {
     @Override
     protected Object run(Object... args) {
-        return null;
+        int[][] grid = (int[][]) args[0];
+        return solution1(grid);
+    }
+
+    // 动态规划,空间复杂度[O(mn)]
+    protected int solution1(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    // 动态规划,空间复杂度[O(1)]
+    protected int solution2(int[][] grid) {
+
+        for (int i = 1; i < grid.length; i++) {
+            grid[i][0] = grid[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < grid[0].length; i++) {
+            grid[0][i] = grid[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
     }
 }
