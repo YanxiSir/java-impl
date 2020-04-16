0d3ad13b961c81dcbc3c7d7c6b47706d6410f4b3
YanxiSir <yanxisir@gmail.com>
2020-04-16 23:22:36 +0800

    fix

--- src/main/java/com/yanxisir/leetcode/medium/Q221.java
+++ src/main/java/com/yanxisir/leetcode/medium/Q221.java
@@ -1,6 +1,9 @@
 package com.yanxisir.leetcode.medium;
 
 import com.yanxisir.leetcode.AbstractQ;
+import com.yanxisir.leetcode.zannos.DynamicPrograming;
+import com.yanxisir.leetcode.zannos.Remember;
+import com.yanxisir.leetcode.zannos.Tag;
 
 /**
  * 最大正方形
@@ -10,11 +13,105 @@ import com.yanxisir.leetcode.AbstractQ;
  * @Date: 2020/2/21
  * @Sign: 心中田间，木行水上
  */
-@Deprecated
+@Remember
+@Tag({"正方形动态规划"})
+@DynamicPrograming
 public class Q221 extends AbstractQ {
 
     @Override
     protected Object run(Object... args) {
-        return null;
+        char[][] matrix = (char[][]) args[0];
+        return solution1(matrix);
+    }
+
+    // 暴力, 有问题
+    protected int solution1(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int ret = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; i < col; j++) {
+                if (matrix[i][j] != 1) {
+                    continue;
+                }
+                ret = Math.max(ret, 1);
+                int rleft = row - i - 1;
+                int lleft = col - j - 1;
+                int step = Math.min(rleft, lleft);
+                for (int m = 1; m <= step; m++) {
+                    // 行都是1,列=j+m
+                    boolean haveZero = false;
+                    for (int t = i + 1; t <= i + m; t++) {
+                        if (matrix[t][j + m] != 1) {
+                            haveZero = true;
+                            break;
+                        }
+                    }
+                    // 列都是1,行=i+m
+                    for (int t = j + 1; t <= j + m; t++) {
+                        if (matrix[i + m][t] != 1) {
+                            haveZero = true;
+                            break;
+                        }
+                    }
+                    if (haveZero) {
+                        break;
+                    } else {
+                        ret = Math.max(m + 1, ret);
+                    }
+                }
+            }
+        }
+        return ret * ret;
+    }
+
+    // 动态规划
+    protected int solution2(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int[][] dp = new int[row + 1][col + 1];
+        int max = 0;
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+
+    // 动态规划 - 空间优化
+    protected int solution3(char[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] dp = new int[col + 1];
+        int max = 0;
+        int pre = 0;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                int tmp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j - 1], dp[j]), pre) + 1;
+                    max = Math.max(max, dp[j]);
+                } else {
+                    dp[j] = 0;
+                }
+                pre = tmp;
+            }
+        }
+        return max * max;
     }
 }
--- src/main/java/com/yanxisir/leetcode/medium/Q337.java
+++ src/main/java/com/yanxisir/leetcode/medium/Q337.java
@@ -1,6 +1,12 @@
 package com.yanxisir.leetcode.medium;
 
 import com.yanxisir.leetcode.AbstractQ;
+import com.yanxisir.leetcode.zannos.DynamicPrograming;
+import com.yanxisir.leetcode.zannos.Tag;
+import com.yanxisir.leetcode.zmodel.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 打家劫舍 III
@@ -10,10 +16,58 @@ import com.yanxisir.leetcode.AbstractQ;
  * @Date: 2020/4/15
  * @Sign: 心中田间，木行水上
  */
-@Deprecated
+@Tag({"树形动态规划"})
+@DynamicPrograming
 public class Q337 extends AbstractQ {
     @Override
     protected Object run(Object... args) {
-        return null;
+        TreeNode root = (TreeNode) args[0];
+        return solution1(root);
+    }
+
+    // 递归+缓存重复子问题的解
+    protected int solution1(TreeNode root) {
+        Map<TreeNode, Integer> memo = new HashMap<>();
+        return s1(root, memo);
+    }
+
+    private int s1(TreeNode node, Map<TreeNode, Integer> memo) {
+        if (node == null) {
+            return 0;
+        }
+        if (memo.containsKey(node)) {
+            return memo.get(node);
+        }
+        // 爷爷
+        int money = node.val;
+        if (node.left != null) {
+            // 孙子
+            money += s1(node.left.left, memo) + s1(node.left.right, memo);
+        }
+        if (node.right != null) {
+            // 孙子
+            money += s1(node.right.left, memo) + s1(node.right.right, memo);
+        }
+        int ret = Math.max(money, s1(node.left, memo) + s1(node.right, memo));
+        memo.put(node, ret);
+        return ret;
+    }
+
+    // 0-不偷；1-偷
+    protected int solution2(TreeNode root) {
+        int[] ret = s2(root);
+        return Math.max(ret[0], ret[1]);
+    }
+
+    private int[] s2(TreeNode node) {
+        if (node == null) {
+            return new int[2];
+        }
+        int[] ret = new int[2];
+        int[] left = s2(node.left);
+        int[] right = s2(node.right);
+        ret[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        ret[1] = left[0] + right[0] + node.val;
+        return ret;
     }
 }
